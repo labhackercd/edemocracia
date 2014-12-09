f23f01dc0afff76da887edfcb9bfe41e6d08d34d
Câmara dos Deputados <wylliam.novais@camara.leg.br>
2014-12-09 09:38:17 -0200

    Deletando para adicionar uma versão mais atual

--- cd-assinaturaforum-portlet/src/main/webapp/html/js/liferay/service.js
+++
@@ -1,9 +0,0 @@
-Liferay.Service.register("Liferay.Service.exportacaoforum", "br.gov.camara.edemocracia.portlets.exportacao.service");
-
-Liferay.Service.registerClass(
-	Liferay.Service.exportacaoforum, "DadosForum",
-	{
-		getDadosForumExportacao: true,
-		getDadosForumAdminExportacao: true
-	}
-);
