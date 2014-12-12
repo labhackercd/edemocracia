b0a591def8a115b11cb6e3189cff282daac3701b
Câmara dos Deputados <wylliam.novais@camara.leg.br>
2014-12-12 10:32:14 -0200

    Adicionando declaração da permissão de cópia e avisos de copyright.

--- cd-dashboard-portlet/src/main/java/br/gov/camara/edemocracia/portlets/dashboard/service/DashboardLocalServiceWrapper.java
+++ cd-dashboard-portlet/src/main/java/br/gov/camara/edemocracia/portlets/dashboard/service/DashboardLocalServiceWrapper.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2009-2014 Câmara dos Deputados. Todos os direitos reservados.
+ *
+ * e-Democracia é um software livre; você pode redistribuí-lo e/ou modificá-lo dentro
+ * dos termos da Licença Pública Geral Menor GNU como publicada pela Fundação do 
+ * Software Livre (FSF); na versão 2.1 da Licença, ou (na sua opinião) qualquer versão.
+ *
+ * Este programa é distribuído na esperança de que possa ser  útil, mas SEM NENHUMA GARANTIA;
+ * sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+ * Veja a Licença Pública Geral Menor GNU para maiores detalhes. 
+ */
 package br.gov.camara.edemocracia.portlets.dashboard.service;
 
 import com.liferay.portal.service.ServiceWrapper;
--- cd-wikilegis-portlet/src/main/java/br/gov/camara/edemocracia/portlets/wikilegis/NoSuchEstruturaException.java
+++ cd-wikilegis-portlet/src/main/java/br/gov/camara/edemocracia/portlets/wikilegis/NoSuchEstruturaException.java
@@ -1,15 +1,13 @@
 /**
- * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2009-2014 Câmara dos Deputados. Todos os direitos reservados.
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * e-Democracia é um software livre; você pode redistribuí-lo e/ou modificá-lo dentro
+ * dos termos da Licença Pública Geral Menor GNU como publicada pela Fundação do 
+ * Software Livre (FSF); na versão 2.1 da Licença, ou (na sua opinião) qualquer versão.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * Este programa é distribuído na esperança de que possa ser  útil, mas SEM NENHUMA GARANTIA;
+ * sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+ * Veja a Licença Pública Geral Menor GNU para maiores detalhes. 
  */
 
 package br.gov.camara.edemocracia.portlets.wikilegis;
